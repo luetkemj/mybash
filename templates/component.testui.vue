56f15fabf1d7171159c6aa2dbb6e1107763ae463
luetkemj <luetkemj@gmail.com>
2022-10-25 10:51:09 -0600

    templates

---
+++ templates/component.testui.vue
@@ -0,0 +1,11 @@
+<template><%NAME% /></template>
+
+<script>
+import %NAME% from '../%NAME%.vue'
+
+export default {
+    name: '%NAME%TestUi',
+
+    components: { %NAME% },
+}
+</script>
---
+++ templates/component.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>%NAME%</div>
+</template>
+
+<script>
+export default {
+	name: "%NAME%",
+
+	props: {},
+
+	data() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
